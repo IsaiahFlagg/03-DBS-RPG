f8295b684c39613f09589ef5757a74e557d59609
I Flagg <isaiahnflagg@gmail.com>
2020-11-21 20:28:56 -0500

    added character picker functionality

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 /////////// VARIABLES ///////////////////
 const characterPicker = $(".pickCharacter")
 const enemyCharacterPicker = $(".pickEnemyCharacter")
-const characterOne = $(".charactersGood1")
-const characterTWO = $(".charactersGood2")
-const characterThree = $(".charactersGood3")
+const characterOne = $("#gokumui")
+const characterTWO = $("#vegeta")
+const characterThree = $("#gotenks")
 let chosenFighter = $("#goodFighter")
 const chosenEnemy = $("#badFighter")
 let goodGuysDiv = $("#goodGuys")
-
-
-
-console.log();
 
 
 
@@ -23,23 +19,41 @@ console.log();
 $("#attackBtn").on("click", function () {
     alert("attacked!")
 })
-$("#charactersGood1").on("click", function () {
-
-    
-    $("#goodFighter").append(`<img src="./img/gokumui.jpg" alt="" class="pickCharacter" id="charactersGood1">`)
+$("#gokumui").on("click", function () {
+    $("#goodFighter").append(`<img src="./img/gokumui.jpg" alt="" class="pickCharacter" id="gokumui"> <span>health</span>`)
+    $("#gokumui").css('display', 'none');
+    $("#deleteSpan").text(" ")
 })
-$("#charactersGood2").on("click", function () {
-    alert("character chosen")
-
-})
-$("#charactersGood3").on("click", function () {
-    alert("character chosen")
+$("#vegeta").on("click", function () {
+    $("#goodFighter").append(`<img src="./img/vegeta.jpg" alt="" class="pickCharacter" id="vegeta"> <span>health</span>`)
+    $("#vegeta").css('display', 'none');
+    $("#deleteSpan2").text(" ")
 
 })
-$(".pickEnemyCharacter").on("click", function () {
-    alert("enemy chosen")
-})
+$("#gotenks").on("click", function () {
+    $("#goodFighter").append(`<img src="./img/gotenks.jpg" alt="" class="pickCharacter" id="gotenks"> <span>health</span>`)
+    $("#gotenks").css('display', 'none');
+    $("#deleteSpan3").text(" ")
 
+})
+$("#gokublack").on("click", function () {
+    $("#gokublack").css('display', 'none');
+    $("#badFighter").append(`<img src="./img/gokublack.jpg" alt="" class="pickEnemyCharacter" id="gokublack"> 
+    <span>health</span>`)
+    $("#deleteSpan4").text(" ")
+})
+$("#broly").on("click", function () {
+    $("#broly").css('display', 'none');
+    $("#badFighter").append(`<img src="./img/broly.jpg" alt="" class="pickEnemyCharacter" id="broly"> 
+    <span>health</span>`)
+    $("#deleteSpan5").text(" ")
+})
+$("#frieza").on("click", function () {
+    $("#frieza").css('display', 'none');
+    $("#badFighter").append(`<img src="./img/frieza.jpg" alt="" class="pickEnemyCharacter" id="frieza"> 
+    <span>health</span>`)
+    $("#deleteSpan6").text(" ")
+})
 
 
 
